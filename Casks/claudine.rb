69bc510fa2e55f40b155d91636f74f303e7fe29a
Florian Ilch <post@florianilch.de>
2025-11-11 23:31:45 +0100

    Brew cask update for claudine version v0.4.0

--- Casks/claudine.rb
+++ Casks/claudine.rb
@@ -2,7 +2,7 @@ cask "claudine" do
   name "claudine"
   desc "Unlock your Claude Pro/Max subscription in any tool or library."
   homepage "https://github.com/florianilch/claudine-proxy"
-  version "0.3.0"
+  version "0.4.0"
 
   livecheck do
     skip "Auto-generated on release."
@@ -12,23 +12,23 @@ cask "claudine" do
 
   on_macos do
     on_intel do
-      url "https://github.com/florianilch/claudine-proxy/releases/download/v0.3.0/claudine_#{version}_darwin_amd64.tar.gz"
-      sha256 "00d3e1374a4148070e4e32168b3b024362fe1a66bbc543ccba1aef48eba03bf8"
+      url "https://github.com/florianilch/claudine-proxy/releases/download/v0.4.0/claudine_#{version}_darwin_amd64.tar.gz"
+      sha256 "9801a878ac212ba25fdf2d435dc890091c3230e226921c516ea30b8c932ab2a7"
     end
     on_arm do
-      url "https://github.com/florianilch/claudine-proxy/releases/download/v0.3.0/claudine_#{version}_darwin_arm64.tar.gz"
-      sha256 "3f89de62077149732c483c3fa49e7ad79cb53ec6c79dfb89393a0b45fa99b344"
+      url "https://github.com/florianilch/claudine-proxy/releases/download/v0.4.0/claudine_#{version}_darwin_arm64.tar.gz"
+      sha256 "40e5e6405ac88b5345f1752be0877afce9bd71e936595aaa3c5972a9df1c0beb"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/florianilch/claudine-proxy/releases/download/v0.3.0/claudine_#{version}_linux_amd64.tar.gz"
-      sha256 "89da3f8e3b675f07b7c382d20c91dbf888e7223d7f165c60719b96d7fec9aaa3"
+      url "https://github.com/florianilch/claudine-proxy/releases/download/v0.4.0/claudine_#{version}_linux_amd64.tar.gz"
+      sha256 "b0307c581936324392757eb08a85f9eb59cbdd94908fe242392060fdc61466d1"
     end
     on_arm do
-      url "https://github.com/florianilch/claudine-proxy/releases/download/v0.3.0/claudine_#{version}_linux_arm64.tar.gz"
-      sha256 "76e18978ffdf05b9904e22aa6dbeb164af14ddfb02ca8e853961d79e51d79457"
+      url "https://github.com/florianilch/claudine-proxy/releases/download/v0.4.0/claudine_#{version}_linux_arm64.tar.gz"
+      sha256 "2d361796e2d31ab8ff820cbd8f909990a8f94d3295025543687e603103edc3eb"
     end
   end
 
